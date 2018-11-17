17b02ea546426e0cd12a7e7aea3e167e9f96270d
djiang2001 <djiang2@stuy.edu>
2018-11-17 14:08:43 -0500

    init 2

--- main.c
+++ main.c
@@ -4,19 +4,32 @@
 #include <unistd.h>
 
 int main(){
-  printf("Testing fork()\n");
+  printf("Parent fork()\n");
   int f = fork();
-  if(f){
-    int k = fork();
-    if(!k){
-      printf("%d \n",getpid());
-      sleep(rand()*15 + 5);
-      printf("Finished \n");
-    }
-  }else{
-    printf("%d \n",getpid());
-     sleep(rand()*15 + 5);
+  int status;
+  int pid = wait(&status);
+
+  if(!f){
+    printf("Child Process: %d Running\n",getpid());//child
+    int rando = rand()%15+5;
+    sleep(rando);
+    printf("Child %d finished after %d seconds\n",getpid(), rando);
+    return rando;
   }
-  
+   printf("Parent wait for child %d for %d seconds \n", pid, WEXITSTATUS(status));
+  printf("Parent fork()\n");
+  int g = fork();
+  int status1;
+  int pid1 = wait(&status1);
+  if(!g){
+    printf("Child Process: %d Running\n",getpid());//child
+    int rando = rand()%15+5;
+    sleep(rando);
+    printf("Child finished after %d seconds\n", rando);
+    return rando;
+  }
+   printf("Parent wait for child %d for %d seconds \n", pid1, WEXITSTATUS(status1));
+ 
+  printf("Parent done \n");
   return 0;
 }
